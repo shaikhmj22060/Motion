8f7c6d2727581aa3a2ea99c7158dc49a14cd8e99
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-03-27 15:52:38 +0530

    created routes for specific Units what i learned

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router';
 import MotionHooks from './MotionHooks';
+import App from './App';
 
 const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<MotionHooks />} />
+      <Route path="/app" element={<App />} />
     </Routes>
   );
 };
