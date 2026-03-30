b74263fc0df766a20eda9db25558e319b6f97ff1
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-03-30 13:35:26 +0530

    layout id learned

---
+++ src/LayoutMotion.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cards from './Motion-Layout/Cards';
+
+const LayoutMotion = () => {
+  return (
+    <div className='flex flex-col items-center bg-neutral-100 justify-center w-full mx-auto'>
+        
+      <Cards />
+    </div>
+  );
+};
+
+export default LayoutMotion;
