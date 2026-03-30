ea9caa55ae346e8dc81e6821c8a2acad235dcaad
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-03-30 13:36:36 +0530

    layoutmotion component configured in routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import MotionHooks from './MotionHooks';
 import App from './App';
+import LayoutMotion from './LayoutMotion';
 
 const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<MotionHooks />} />
       <Route path="/app" element={<App />} />
+      <Route path="/layout" element={<LayoutMotion />} />
     </Routes>
   );
 };
