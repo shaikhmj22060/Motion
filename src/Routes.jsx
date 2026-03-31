a2afe3f14dedcc0dfeec1593e58942e0258d0830
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-03-31 23:49:25 +0530

    text animation component added to the route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router';
 import MotionHooks from './MotionHooks';
 import App from './App';
 import LayoutMotion from './LayoutMotion';
+import AnimateSequence from './AnimateSequence';
+import Courasoule from './Courasoule';
+import Text from './Text';
 
 const RoutesComponent = () => {
   return (
@@ -9,6 +12,9 @@ const RoutesComponent = () => {
       <Route path="/" element={<MotionHooks />} />
       <Route path="/app" element={<App />} />
       <Route path="/layout" element={<LayoutMotion />} />
+      <Route path="/animateSequence" element={<AnimateSequence />} />
+      <Route path="/courasole" element={<Courasoule />} />
+      <Route path="/text" element={<Text />} />
     </Routes>
   );
 };
